c10c1e1560153129daae82bef5ecf8d981fd8a8e
Nair-Abhinav <abhinavnair7404@gmail.com>
2025-01-04 12:21:41 +0530

    implement Merge Sort algorithm

---
+++ Sorting algos/MergeSort.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(int arr[], int start, int mid, int end)
+{
+    int n1 = mid - start + 1;
+    int n2 = end - mid;
+
+    vector<int> left(n1), right(n2);
+
+    for (int i = 0; i < n1; i++)
+        left[i] = arr[start + i];
+    for (int j = 0; j < n2; j++)
+        right[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = start;
+    while (i < n1 && j < n2)
+    {
+        if (left[i] <= right[j])
+        {
+            arr[k] = left[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = left[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = right[j];
+        j++;
+        k++;
+    }
+}
+
+void mergesort(int arr[], int start, int end)
+{
+    if (start < end)
+    {
+        int mid = start + (end - start) / 2;
+
+        mergesort(arr, start, mid);
+        mergesort(arr, mid + 1, end);
+
+        merge(arr, start, mid, end);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    mergesort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
