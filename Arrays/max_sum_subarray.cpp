aa3e34b0ced56bc7bb8a48b88e8657c78da085a6
Nair-Abhinav <abhinavnair7404@gmail.com>
2025-01-12 09:52:03 +0530

    Implement divide and conquer approach for maximum subarray sum

--- Arrays/max_sum_subarray.cpp
+++ Arrays/max_sum_subarray.cpp
@@ -25,21 +25,36 @@ using namespace std;
 // 
 
 // kadanes algo
-int maxSubArray(vector<int>& nums) {
-        int max = INT_MIN;
-        int current_sum =0 ;
-        for(int i=0;i<nums.size();i++){
-            current_sum += nums[i];
-            if(current_sum > max){
-                max = current_sum;
-            }
-            if(current_sum < 0){
-                current_sum = 0;
-            }
-        }
-        return max;
-    }
+// int maxSubArray(int arr[], int n) {
+//         int max = INT_MIN;
+//         int current_sum =0 ;
+//         for(int i=0;i<n;i++){
+//             current_sum += arr[i];
+//             if(current_sum > max){
+//                 max = current_sum;
+//             }
+//             if(current_sum < 0){
+//                 current_sum = 0;
+//             }
+//         }
+//         return max;
+//     }
 
+// divide and conquer
+int maxSubArray(int arr[] , int start , int end){
+    if(start == end){
+        return arr[start];
+    }
+    int max = INT_MIN;
+    int mid = (start+end)/2;
+    int s1 = maxSubArray(arr , start , mid);
+    int s2 = maxSubArray(arr , mid+1 , end);
+    int s3 = s1+s2;
+    if(max < s3){
+        max = s3;
+    }
+    return max; 
+}
 int main()
 {
     int n;
@@ -53,7 +68,7 @@ int main()
     }
     // int res = max_sum_subarray_brutforce_TC_On3(arr, n);
     // int res = max_sum_subarray_brutforce_TC_On2(arr, n);
-    int res = maxSubArray(arr);
+    int res = maxSubArray(arr, 0, n - 1);
     cout << "The maximum sum of the subarray is: " << res;
     return 0;
 }
